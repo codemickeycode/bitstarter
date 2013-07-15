614cfc20ec86cfbc3c5adc75f346edec6b0a121d
Mickey <codemickeycode@gmail.com>
2013-07-15 14:58:59 +0000

    added file reader

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+//Reads the file and outputs the contents on the screen
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html','utf8');
   response.send(buf);
